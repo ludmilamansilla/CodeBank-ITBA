41518fb1b755585dc0e602bc631dfc73a5431111
Nicolas Lussoro <lussoro8@gmail.com>
2024-08-25 17:15:04 -0300

    Añadi una animacion a la pagina de inicio

---
+++ public/js/funcion.js
@@ -0,0 +1,30 @@
+function mover(id) {
+    const foto = document.getElementById(id);
+
+    const handleMouseMove = function(event) {
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+
+        const fotoWidth = foto.offsetWidth;
+        const fotoHeight = foto.offsetHeight;
+
+        const fotoCenterX = foto.offsetLeft + fotoWidth / 2;
+        const fotoCenterY = foto.offsetTop + fotoHeight / 2;
+
+        const angleX = (fotoCenterY - mouseY) * 0.004;
+        const angleY = (fotoCenterX - mouseX) * -0.030;
+
+        foto.style.transform = `perspective(1000px) rotateX(${angleX}deg) rotateY(${angleY}deg) scale3d(1,1,1)`;
+    };
+
+    const handleMouseOut = function() {
+        document.removeEventListener('mousemove', handleMouseMove);
+        foto.style.transform = `perspective(500px) rotateX(${0}deg) rotateY(${0}deg) scale3d(1,1,1)`;
+    };
+
+    foto.addEventListener('mouseover', function() {
+        document.addEventListener('mousemove', handleMouseMove);
+    });
+
+    foto.addEventListener('mouseout', handleMouseOut);
+}
